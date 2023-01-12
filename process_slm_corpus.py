3836ad5249fe3b08189bc6b93ae5866892475620
mcps5601 <mcps5601@gmail.com>
2023-01-12 14:45:54 +0800

    Now support choosing raw or slm (following the pre-processing procedure of SLM)

--- process_slm_corpus.py
+++ process_slm_corpus.py
@@ -3,19 +3,34 @@ from tqdm import tqdm
 import pickle
 from prepro.tokenization import CWSTokenizer
 
-tokenizer = CWSTokenizer("prepro/vocab.txt")
 
-tmp_sents = []
-for data in Path("corpus").glob("*.txt"):
-# for data in ["corpus/pku_unsegmented.txt"]:
-    print(f"Now processing {data}.")
-    with open(data, "r") as f:
-        sents = f.read().splitlines()
+def main(process_type):
+    assert process_type in ['slm', 'raw']
+    tokenizer = CWSTokenizer("prepro/vocab.txt")
 
-    for line in tqdm(sents):
-        uscher, token, segment = tokenizer.sent_tokenize(line)
-        tmp_sents.extend(token)
+    tmp_sents = []
+    for data in Path("corpus").glob("*.txt"):
+        print(f"Now processing {data}.")
+        with open(data, "r") as f:
+            sents = f.read().splitlines()
 
-processed_sents = [i for i_list in tmp_sents for i in i_list]
-with open("corpus/processesed.pkl", "wb") as f: 
-    pickle.dump(processed_sents, f)
+        if process_type == 'slm':
+            for line in tqdm(sents):
+                uscher, token, segment = tokenizer.sent_tokenize(line)
+                tmp_sents.extend(token)
+
+        elif process_type == 'raw':
+            tmp_sents.extend(sents)
+
+    if process_type == 'slm':
+        processed_sents = [i for i_list in tmp_sents for i in i_list]
+
+    elif process_type == 'raw':
+        processed_sents = tmp_sents
+
+    with open(f"corpus/{process_type}.pkl", "wb") as f: 
+        pickle.dump(processed_sents, f)
+
+if __name__ == '__main__':
+    PROCESS_TYPE = 'slm'
+    main(PROCESS_TYPE)
--- train_cbow.py
+++ train_cbow.py
@@ -4,24 +4,35 @@ import logging
 import pickle
 from gensim.models import word2vec
 
-def main():
+def main(epoch: int, process_type: str):
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    file = open("corpus/processed_correct.pkl", "rb")
+    if process_type == 'slm':
+        file = open("corpus/processed_correct.pkl", "rb")
+    elif process_type == 'raw':
+        file = open("corpus/combined.pkl", "rb")
     sentences = pickle.load(file)
+
     print(f"Total number of sentences: {len(sentences)}")
     # sentences = word2vec.PathLineSentences("corpus")
     model = word2vec.Word2Vec(
         sentences,
+        sg=0,
         vector_size=256,
-        epochs=32,
+        window=5,
+        min_count=5,
+        epochs=epoch,
+        hs=0,
+        negative=5,
+        sample=1e-4,
+        workers=20,
     )
 
     #保存模型，供日後使用
-    model.save("word2vec.model")
-
-    #模型讀取方式
-    model = word2vec.Word2Vec.load("your_model_name")
+    save_name = f"cbow_wiki_msr_pku_e{epoch}_{process_type}.model"
+    model.save(save_name)
 
 if __name__ == '__main__':
-    main()
+    PROCESS_TYPE = 'slm'
+    EPOCH = 50
+    main(EPOCH, PROCESS_TYPE)
